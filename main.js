9e2a86d34c41ef626429d149d717f418220290aa
Zinovatniy-Ilya <zinot2006@gmail.com>
2022-04-25 16:15:48 +0300

    1

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 const gallery = document.querySelector('.gallery')
-const newTechnologies = [];
+const newTechnologies = ["buy"];
 const button = newTechnologies.map((technology) => `<li class="item"><button claas="btn">${technology}</button></li>`).join("");
     gallery.insertAdjacentHTML("beforeend", button);
